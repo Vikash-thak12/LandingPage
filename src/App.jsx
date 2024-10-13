f486094f46d14c4b01bd612c96f98d9be684c049
Vikash Thakur <thakvikash622@gmail.com>
2024-10-13 23:30:36 +0530

    Basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import NavbarPage from "./components/Navbar"
+
 function App() {
 
   return (
     <>
-     <h1>Hey yo</h1>
+     <NavbarPage />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Camera } from 'lucide-react';
+const NavbarPage = () => {
+  return (
+    <div>
+      <h1>This is Navbar page</h1>
+      <Camera color="black" size={48} />;
+    </div>
+  );
+};
+
+export default NavbarPage;
