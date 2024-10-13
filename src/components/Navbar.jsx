6065d04ca9aff0f23dfce5e595b63b4e1ba0b917
Vikash Thakur <thakvikash622@gmail.com>
2024-10-13 23:56:43 +0530

    Fixed: Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,34 +7,13 @@ const NavbarPage = () => {
     const toggleNavbar = () => {
         setMobileDrawerOpen(!moblieDrawerOpen)
     }
-  return (
-    <main>
-        <div className="border flex items-center justify-between px-10 py-5">
-            <div>
-                <h1>Vikash</h1>
-            </div>
-            <div className="hidden md:flex items-center gap-5">
-                {
-                    navItems.map((item, index) => (
-                        <li key={index} className="list-none">
-                            <a href={item.href}>{item.label}</a>
-                        </li>
-                    ))
-                }
-            </div>
-            <div className="hidden md:flex items-center gap-5">
-                <button>login</button>
-                <button>Create Account</button>
-            </div>
-            <div onClick={toggleNavbar} className="md:hidden flex cursor-pointer">
-                {
-                    moblieDrawerOpen ? <X /> : <Menu />
-                }
-            </div>
-        </div>
-        {
-            moblieDrawerOpen && (
-                <div className="fixed right-0 z-50 h-full bg-neutral-900 p-12 flex flex-col gap-5 items-start lg:hidden">
+    return (
+        <main>
+            <div className="border flex items-center justify-between px-10 py-5">
+                <div>
+                    <h1>Vikash</h1>
+                </div>
+                <div className="hidden md:flex items-center gap-5">
                     {
                         navItems.map((item, index) => (
                             <li key={index} className="list-none">
@@ -43,10 +22,35 @@ const NavbarPage = () => {
                         ))
                     }
                 </div>
-            )
-        }
-    </main>
-  );
+                <div className="hidden md:flex items-center gap-5">
+                    <button>login</button>
+                    <button>Create Account</button>
+                </div>
+                <div onClick={toggleNavbar} className="md:hidden flex cursor-pointer">
+                    {
+                        moblieDrawerOpen ? <X /> : <Menu />
+                    }
+                </div>
+            </div>
+            {
+                moblieDrawerOpen && (
+                    <div className="fixed right-0 z-50 h-full bg-neutral-900 p-12 flex flex-col gap-5 items-start md:hidden">
+                        {
+                            navItems.map((item, index) => (
+                                <li key={index} className="list-none">
+                                    <a href={item.href}>{item.label}</a>
+                                </li>
+                            ))
+                        }
+                        <div className="md:hidden flex flex-col items-start gap-5">
+                            <button className="border p-3 rounded-xl">login</button>
+                            <button className="p-3 rounded-xl bg-orange-500 text-black font-bold">Create Account</button>
+                        </div>
+                    </div>
+                )
+            }
+        </main>
+    );
 };
 
 export default NavbarPage;
