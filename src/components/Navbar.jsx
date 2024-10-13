8d4829b03d2f1b62a0b4b7dd0b0ef3f5c243c3ef
Vikash Thakur <thakvikash622@gmail.com>
2024-10-13 23:39:43 +0530

    Updated necessary codes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,33 @@
-import { Camera } from 'lucide-react';
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 const NavbarPage = () => {
+    const [moblieDrawerOpen, setMobileDrawerOpen] = useState(false);
+    const toggleNavbar = () => {
+        setMobileDrawerOpen(!moblieDrawerOpen)
+    }
   return (
-    <div>
-      <h1>This is Navbar page</h1>
-      <Camera color="black" size={48} />;
-    </div>
+    <main>
+        <div className="border flex items-center justify-between px-10 py-5">
+            <div>
+                <h1>Vikash</h1>
+            </div>
+            <ul className="hidden md:flex items-center justify-center gap-5 ">
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+            <div className="hidden md:flex items-center gap-5">
+                <button>login</button>
+                <button>Create Account</button>
+            </div>
+            <div onClick={toggleNavbar} className="md:hidden flex cursor-pointer">
+                {
+                    moblieDrawerOpen ? <X /> : <Menu />
+                }
+            </div>
+        </div>
+    </main>
   );
 };
 
