860aa0c4de799344da0bb5e4e49f7542f6bd46b7
Vikash Thakur <thakvikash622@gmail.com>
2024-10-14 00:45:14 +0530

    Fixed: Featuersection

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -1,7 +1,28 @@
+import { features } from "../constants";
+
+
 const FeatureSection = () => {
   return (
-    <div className="mt-10">
-      <h1>This is Feature Section</h1>
+    <div className="my-10">
+      <div>
+        <h1 className="text-center text-4xl font-bold">Feature</h1>
+        <p className="text-center text-xl mt-5">Enhance the feature of my website</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5 mt-5">
+            {
+                features.map((item, index) => (
+                    <div key={index} className="p-3 flex gap-5 cursor-pointer rounded-2xl bg-gray-800 hover:scale-105 transition-all">
+                        <div className="mt-1">
+                            {item.icon}
+                        </div>
+                        <div>
+                            <h1 className="font-bold text-xl">{item.text}</h1>
+                            <p className="text-gray-400">{item.description}</p>
+                        </div>
+                    </div>
+                ))
+            }
+        </div>
+      </div>
     </div>
   );
 };
